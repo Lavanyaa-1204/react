492b1d8350d0b339ab71864504dfaf3bd9da68c7
Lavanyaa-1204 <lavanyaajagm@gmail.com>
2025-01-11 22:33:43 +0530

    Create header.jsx

---
+++ header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <header className="header">
+            <img className="globe" height= "40px" src="https://www.freeiconspng.com/uploads/globe-icon-28.png" alt="globe"/>
+            <div>my travel journal.</div>
+
+        </header>
+    )
+}
